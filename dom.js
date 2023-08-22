47d2e042eee1678e69e0cf6fead2704111a811c3
hrd05 <harshdunkhwal55@gmail.com>
2023-08-22 18:49:25 +0530

    second commit

---
+++ dom.js
@@ -0,0 +1,8 @@
+const header= document.getElementById('main-header');
+//headerTitle.innerText='harsh'; //or  use textContent
+//console.log(headerTitle.innerHTML);
+header.style.border='solid 3px #000';
+
+const title = document.querySelector('.title');
+title.style.fontWeight='bold';
+title.style.color='green';
