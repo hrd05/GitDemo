4fcb9d180fd44e67c3623eb56066e867cae0e89c
hrd05 <harshdunkhwal55@gmail.com>
2023-08-22 21:26:24 +0530

    learned querySelector

--- dom.js
+++ dom.js
@@ -20,13 +20,27 @@
 // }
 // li element with no class doent change
 // now use of get element by tag name
-var li = document.getElementsByTagName('li');
 
-//console.log(items);
 
-li[2].style.background='green';
+// var li = document.getElementsByTagName('li');
 
-for(var i=0;i<li.length;i++){
-    li[i].style.fontWeight = 'bold';
-    //li[i].style.background = 'yellow';
+// //console.log(items);
+
+// li[2].style.background='green';
+
+// for(var i=0;i<li.length;i++){
+//     li[i].style.fontWeight = 'bold';
+//     //li[i].style.background = 'yellow';
+// }
+
+// queryselctorall
+
+var items = document.querySelectorAll('.list-group-item');
+var odd = document.querySelectorAll('li:nth-child(odd)');
+
+items[1].style.color='green';
+//items[2].style.display='none';
+
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='green';
 }
