9015a27779941edc0cb8f993577db7dfadfd8f2e
hrd05 <harshdunkhwal55@gmail.com>
2023-08-22 20:24:06 +0530

    third commit

--- dom.js
+++ dom.js
@@ -1,8 +1,20 @@
-const header= document.getElementById('main-header');
-//headerTitle.innerText='harsh'; //or  use textContent
-//console.log(headerTitle.innerHTML);
-header.style.border='solid 3px #000';
+// const header= document.getElementById('main-header');
+// //headerTitle.innerText='harsh'; //or  use textContent
+// //console.log(headerTitle.innerHTML);
+// header.style.border='solid 3px #000';
 
-const title = document.querySelector('.title');
-title.style.fontWeight='bold';
-title.style.color='green';
+// const title = document.querySelector('.title');
+// title.style.fontWeight='bold';
+// title.style.color='green';
+
+//GetElementsByClassName
+
+var items = document.getElementsByClassName('list-group-item');
+
+//console.log(items);
+
+items[2].style.background='green';
+
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight = 'bold';
+}
