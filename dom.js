19a8f08d644e8f522d94f949c1615027959109c7
hrd05 <harshdunkhwal55@gmail.com>
2023-08-22 20:57:49 +0530

    3rd commit (get element by tag name)

--- dom.js
+++ dom.js
@@ -9,12 +9,24 @@
 
 //GetElementsByClassName
 
-var items = document.getElementsByClassName('list-group-item');
+// var items = document.getElementsByClassName('list-group-item');
+
+// //console.log(items);
+
+// items[2].style.background='green';
+
+// for(var i=0;i<items.length;i++){
+//     items[i].style.fontWeight = 'bold';
+// }
+// li element with no class doent change
+// now use of get element by tag name
+var li = document.getElementsByTagName('li');
 
 //console.log(items);
 
-items[2].style.background='green';
+li[2].style.background='green';
 
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight = 'bold';
+for(var i=0;i<li.length;i++){
+    li[i].style.fontWeight = 'bold';
+    //li[i].style.background = 'yellow';
 }
